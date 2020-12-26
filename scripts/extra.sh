52e9bc041ecbbe2c01457ebd90bcff54343e1a9e
tudi1909 <vip029@me.com>
2020-12-26 11:05:57 +0800

    Update extra.sh

--- scripts/extra.sh
+++ scripts/extra.sh
@@ -8,18 +8,18 @@ rm -rf lean luci-lib-docker luci-app-docker luci-app-diskman autocore parted luc
 [ -f luci-app-ttyd/root/etc/init.d/ttyd ] && rm luci-app-ttyd/root/etc/init.d/ttyd
 [ -f luci-app-rclone/luasrc/controller/rclone.lua ] && sed -i '/firstchild/Id;s/nas/services/g' luci-app-rclone/luasrc/controller/rclone.lua
 
+# VSSR
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+
 # autocore
 cp -r ../../../package/autocore autocore
-
-# luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 
 # ServerChan
 git clone https://github.com/tty228/luci-app-serverchan
 
-#VSSR
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
+# Luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 
 # dockerman
 #svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
