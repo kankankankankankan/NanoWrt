f6da9ae763467deccbcf36ed9d96f5c72738ac79
tudi1909 <vip029@me.com>
2021-01-03 10:59:47 +0800

    Fix

--- scripts/extra.sh
+++ scripts/extra.sh
@@ -4,7 +4,7 @@ cd package
 # lean
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean
 cp -r lean/* ./
-rm -rf lean luci-lib-docker luci-app-docker luci-app-diskman autocore parted luci-theme-argon
+rm -rf lean luci-lib-docker luci-app-docker luci-app-diskman autocore parted luci-theme-argon parted
 [ -f luci-app-ttyd/root/etc/init.d/ttyd ] && rm luci-app-ttyd/root/etc/init.d/ttyd
 [ -f luci-app-rclone/luasrc/controller/rclone.lua ] && sed -i '/firstchild/Id;s/nas/services/g' luci-app-rclone/luasrc/controller/rclone.lua
 
@@ -21,6 +21,13 @@ git clone https://github.com/tty228/luci-app-serverchan
 # Luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 
+# AdGuard Home
+git clone https://github.com/kankankankankankan/luci-app-adguardhome.git
+
+# JD-dailybonus
+git clone https://github.com/jerrykuku/node-request.git
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+
 # dockerman
 #svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
 #svn co https://github.com/lisaac/luci-lib-docker/trunk/collections/luci-lib-docker
@@ -35,4 +42,8 @@ cd ../
 #wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
 
 # hello world
-sed -i "/routing/a\\$(echo "U2FsdGVkX1+AeI7cP72nGJzrdtGxFrYZW+kKEANWGZryYXLSls7b7Z3awocMj1hEJ15w20FKz2msgAoTnYyILpGZKHr+nxL/GoilV5oHp8Q=" | openssl enc -aes-256-cbc -a -d -pass pass:"vinewx" -pbkdf2)" feeds.conf.default
+#sed -i "/routing/a\\$(echo "U2FsdGVkX1+AeI7cP72nGJzrdtGxFrYZW+kKEANWGZryYXLSls7b7Z3awocMj1hEJ15w20FKz2msgAoTnYyILpGZKHr+nxL/GoilV5oHp8Q=" | openssl enc -aes-256-cbc -a -d -pass pass:"vinewx" -pbkdf2)" feeds.conf.default
+
+# passwall
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
+echo 'src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
